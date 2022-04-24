7e8e60fe8c49530dcff03bc42109d8c81feb891e
Vinícius Formigone <formiga5557@gmail.com>
2022-04-24 00:56:35 -0300

    Added missing user's password field

--- API/Models/User.cs
+++ API/Models/User.cs
@@ -7,5 +7,7 @@ namespace API.Models
         public int Id { get; set; }
         [Required, MaxLength(30)]
         public string UserName { get; set; } = null!;
+        [Required, MaxLength(30)]
+        public string Password { get; set; } = null!;
     }
 }
